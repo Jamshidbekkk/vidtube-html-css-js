c6f0913c0338dd16a60cf3d055d9c165c9c792a6
Jamshidbek <kamoliddinovjamshid7@gmail.com>
2024-02-01 02:51:48 +0500

    sidebar done

---
+++ main.js
@@ -0,0 +1,9 @@
+let menuIcon = document.querySelector('.menu-icon');
+let sidebar = document.querySelector('.sidebar');
+
+
+
+
+menuIcon.onclick = () => {
+    sidebar.classList.toggle('small-sidebar');
+}
