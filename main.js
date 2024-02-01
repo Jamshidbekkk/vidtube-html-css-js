d4cba18707936be4a88a739f1f2cd7f269bfab6c
Jamshidbek <kamoliddinovjamshid7@gmail.com>
2024-02-01 22:36:48 +0500

    homepage done with its media query

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 let menuIcon = document.querySelector('.menu-icon');
 let sidebar = document.querySelector('.sidebar');
 
+let container = document.querySelector('.container');
+
+
 
 
 
 menuIcon.onclick = () => {
     sidebar.classList.toggle('small-sidebar');
+    container.classList.toggle('large-container');
 }
